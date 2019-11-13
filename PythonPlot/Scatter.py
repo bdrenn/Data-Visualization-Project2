0b2a982f21743bfca5d659db519899e2958ba62c
bdrenn <briansdrennan@gmail.com>
2019-11-12 19:37:15 -0800

    annotation aligned

--- PythonPlot/Scatter.py
+++ PythonPlot/Scatter.py
@@ -46,6 +46,13 @@ scanpath_graph_general = graph_general_data.ScanPathLength() # X-axis
 task_success_graph_general = graph_general_data.TaskSuccess() # Y-axis
 
 
+#High Scores     
+high_score_tree_expert = task_success_tree_expert.max() 
+high_score_tree_general = task_success_tree_general.max()
+high_score_graph_expert = task_success_graph_expert.max()
+high_score_graph_general = task_success_graph_general.max()
+
+
 # Plot
 fig = go.Figure()
 
@@ -54,6 +61,9 @@ fig.add_trace(
     go.Scatter(x=scanpath_graph_expert,
                y=task_success_graph_expert,
                mode='markers',
+               marker=dict(
+                   size=20
+               ),
                name="Expert Graph"))
 
 fig.add_trace(
@@ -61,12 +71,18 @@ fig.add_trace(
                y=task_success_graph_general,
                name="General Graph",
                mode='markers',
+               marker=dict(
+                   size=20
+               ),
                visible=False))
 
 fig.add_trace(
     go.Scatter(x=scanpath_tree_expert,
                y=task_success_tree_expert,
                mode='markers',
+               marker=dict(
+                   size=20
+               ),
                name="Expert Tree"))
 
 fig.add_trace(
@@ -74,27 +90,34 @@ fig.add_trace(
                y=task_success_tree_general,
                name="General Tree",
                mode='markers',
+               marker=dict(
+                   size=20
+               ),
                visible=False))
-
-
-#High Scores     
-high_score_tree_expert = task_success_tree_expert.max() 
-high_score_tree_general = task_success_tree_general.max()
-high_score_graph_expert = task_success_graph_expert.max()
-high_score_graph_general = task_success_graph_general.max()
 
 
 # # Add Annotations and Buttons
 high_annotations_expert = [dict(x="temp",
-                         y=high_score_graph_expert, 
+                         y=high_score_graph_expert,
                          xref="x", yref="y",
                          text="High Score Graph:<br> %.2f" % high_score_graph_expert,
                          ax=0, ay=-40),
                     dict(x="temp",
                          y=high_score_tree_expert,
                          xref="x", yref="y",
-                         text="High Score Tree:<br> %.2f" % high_score_graph_general,
+                         text="High Score Tree:<br> %.2f" % high_score_tree_expert,
                          ax=0, ay=-40)]
+high_annotations_general = [dict(x="temp",
+                         y=high_score_graph_general, 
+                         xref="x", yref="y",
+                         text="High Score Graph:<br> %.2f" % high_score_graph_general,
+                         ax=0, ay=-40),
+                    dict(x="temp",
+                         y=high_score_tree_expert,
+                         xref="x", yref="y",
+                         text="High Score Tree:<br> %.2f" % high_score_tree_general,
+                         ax=0, ay=-40)]
+
 # low_annotations = [dict(x="2015-05-01",
 #                         y=df.Low.mean(),
 #                         xref="x", yref="y",
@@ -115,26 +138,26 @@ fig.update_layout(
             x=0.57,
             y=1.2,
             buttons=list([
-                dict(label="None",
+                dict(label="Expert",
                      method="update",
                      args=[{"visible": [True, False, True, False]},
-                           {'annotations': high_annotations_expert}]),
-                dict(label="Graph",
+                     {"annotations": high_annotations_expert}]),
+                dict(label="General",
                      method="update",
-                     args=[{"visible": [True, True, False, False]}]),
-                dict(label="Tree",
-                     method="update",
-                     args=[{"visible": [False, False, True, True]}]),
+                     args=[{"visible": [False, True, False, True]},
+                     {"annotations": high_annotations_general}]),
                 dict(label="Both",
                      method="update",
-                     args=[{"visible": [True, True, True, True]}]),
+                     args=[{"visible": [True, True, True, True]},
+                     {"annotations": high_annotations_expert}]),
             ]),
         )
     ])
 
 # Set title
 fig.update_layout(
-    title_text="Experiment"
+    title_text="Experiment",
+    margin=dict(l=200),
 )
 
 fig.show()
