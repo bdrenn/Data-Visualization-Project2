0aa4ab7c3cca7fb9cb3fa0774e7f2a4fef3f0acf
bdrenn <briansdrennan@gmail.com>
2019-11-12 16:39:54 -0800

    working scatter with buttons

--- PythonPlot/Scatter.py
+++ PythonPlot/Scatter.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+import plotly
 import numpy as np
 import os 
 import Preprocess as Data
@@ -11,31 +12,128 @@ path = Path(os.getcwd()).parent
 output_data_dir = path / 'OutputData'
 graph_expert_data_dir = output_data_dir / 'GraphExpertData.csv'
 
+#Graph General directory 
+output_data_dir = path / 'OutputData'
+graph_general_data_dir = output_data_dir / 'GraphGeneralData.csv'
+
 #Tree Expert directory 
 output_data_dir = path / 'OutputData'
 tree_expert_data_dir = output_data_dir / 'TreeExpertData.csv'
 
+#Tree General directory 
+output_data_dir = path / 'OutputData'
+tree_general_data_dir = output_data_dir / 'TreeGeneralData.csv'
+
+
 #Tree Expert Data Object 
 tree_expert_data = Data.TreeExpertData(tree_expert_data_dir)
+scanpath_tree_expert = tree_expert_data.ScanPathLength() # X-axis
+task_success_tree_expert = tree_expert_data.TaskSuccess() # Y-axis
 
-scanpath_tree = tree_expert_data.ScanPathLength() # X-axis
-task_success_tree = tree_expert_data.TaskSuccess() # Y-axis
+#Tree General Data Object
+tree_general_data = Data.TreeGeneralData(tree_general_data_dir)
+scanpath_tree_general = tree_general_data.ScanPathLength() # X-axis
+task_success_tree_general = tree_general_data.TaskSuccess() # Y-axis
 
 #Graph Expert Data Object 
 graph_expert_data = Data.GraphExpertData(graph_expert_data_dir)
+scanpath_graph_expert = graph_expert_data.ScanPathLength() # X-axis
+task_success_graph_expert = graph_expert_data.TaskSuccess() # Y-axis
 
-scanpath_graph = graph_expert_data.ScanPathLength() # X-axis
-task_success_graph = graph_expert_data.TaskSuccess() # Y-axis
+#Graph General Data Object
+graph_general_data = Data.GraphGeneralData(graph_general_data_dir)
+scanpath_graph_general = graph_general_data.ScanPathLength() # X-axis
+task_success_graph_general = graph_general_data.TaskSuccess() # Y-axis
 
 
 # Plot
 fig = go.Figure()
 
-fig.add_trace(go.Scatter(x=scanpath_graph, y=task_success_graph,
-                    mode='markers'))
-fig.add_trace(go.Scatter(x=scanpath_tree, y=task_success_tree,
-                    mode='markers' ))
+# Add Traces
+fig.add_trace(
+    go.Scatter(x=scanpath_graph_expert,
+               y=task_success_graph_expert,
+               mode='markers',
+               name="Expert Graph"))
 
+fig.add_trace(
+    go.Scatter(x=scanpath_graph_general,
+               y=task_success_graph_general,
+               name="General Graph",
+               mode='markers',
+               visible=False))
+
+fig.add_trace(
+    go.Scatter(x=scanpath_tree_expert,
+               y=task_success_tree_expert,
+               mode='markers',
+               name="Expert Tree"))
+
+fig.add_trace(
+    go.Scatter(x=scanpath_tree_general,
+               y=task_success_tree_general,
+               name="General Tree",
+               mode='markers',
+               visible=False))
+
+
+#High Scores     
+high_score_tree_expert = task_success_tree_expert.max() 
+high_score_tree_general = task_success_tree_general.max()
+high_score_graph_expert = task_success_graph_expert.max()
+high_score_graph_general = task_success_graph_general.max()
+
+
+# # # Add Annotations and Buttons
+# high_annotations = [dict(x="2016-03-01",
+#                          y=high_score_graph_expert, 
+#                          xref="x", yref="y",
+#                          text="High Score Graph Expert:<br> %.2f" % high_score_graph_expert,
+#                          ax=0, ay=-40),
+#                     dict(x="temp",
+#                          y=high_score_graph_general,
+#                          xref="x", yref="y",
+#                          text="High Score Graph General:<br> %.2f" % high_score_graph_general,
+#                          ax=0, ay=-40)]
+# low_annotations = [dict(x="2015-05-01",
+#                         y=df.Low.mean(),
+#                         xref="x", yref="y",
+#                         text="Low Average:<br> %.2f" % df.Low.mean(),
+#                         ax=-40, ay=40),
+#                    dict(x=df.High.idxmin(),
+#                         y=df.Low.min(),
+#                         xref="x", yref="y",
+#                         text="Low Min:<br> %.2f" % df.Low.min(),
+#                         ax=0, ay=40)]
+
+fig.update_layout(
+    updatemenus=[
+        go.layout.Updatemenu(
+            type="buttons",
+            direction="right",
+            active=0,
+            x=0.57,
+            y=1.2,
+            buttons=list([
+                dict(label="None",
+                     method="update",
+                     args=[{"visible": [True, False, True, False]}]),
+                dict(label="Graph",
+                     method="update",
+                     args=[{"visible": [True, True, False, False]}]),
+                dict(label="Tree",
+                     method="update",
+                     args=[{"visible": [False, False, True, True]}]),
+                dict(label="Both",
+                     method="update",
+                     args=[{"visible": [True, True, True, True]}]),
+            ]),
+        )
+    ])
+
+# Set title
+fig.update_layout(
+    title_text="Experiment"
+)
 
 fig.show()
-
