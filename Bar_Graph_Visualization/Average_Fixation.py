0a30d7fecb16f52570f79f2a15a73bbb198d85fb
Jose <gallardojose444@gmail.com>
2019-11-13 15:20:31 -0800

    Finished_Averages

--- Bar_Graph_Visualization/Average_Fixation.py
+++ Bar_Graph_Visualization/Average_Fixation.py
@@ -3,6 +3,64 @@ import os
 import math
 import sys
 
+def get_Max_Time(source, Time_Interval):
+    Max_Time = 0
+    for filename in os.listdir(source):
+        with open(source + filename, newline='') as csvfile:
+            data = list(csv.reader(csvfile))
+        data = [x for x in data if x != []]
+        Max_Time = max(int(data[-1][1]) + int(data[-1][2]), Max_Time)
+    return math.ceil(Max_Time / Time_Interval)
+
+def mk_Average_Fixation_File(source, target, Max_Time_Minutes, Time_Interval):
+    Array_Avg = []
+    for row in range(Max_Time_Minutes):
+        Array_Avg.append([0,0])
+
+
+    for filename in os.listdir(source):
+        with open(source + filename, newline='') as csvfile:
+            data = list(csv.reader(csvfile))
+        data = [x for x in data if x != []]
+        # increment amount of people only once
+        for minute in range(Max_Time_Minutes):
+            first_time = True
+            for fixation in data:
+                if int(fixation[1]) < ((minute +1) * Time_Interval):
+                    if (((minute + 1) * Time_Interval) - Time_Interval) < int(fixation[1]) < ((minute + 1) * Time_Interval):
+                        if int(fixation[1]) + int(fixation[2]) < ((minute + 1) * Time_Interval):
+                            # print(str(minute) + ' ' + str(fixation) + ' ' + str(minute * Time_Interval - Time_Interval))
+                            # print(int(fixation[1]) + int(fixation[2]))
+                            # increment the amount of people for average
+                            if first_time:
+                                Array_Avg[minute][0] += 1
+                                first_time = False
+                            Array_Avg[minute][1] += int(fixation[2])
+                        else:
+                            # increment the amount of people for average
+                            if first_time:
+                                # Average_Dictionary[minute][0] += 1
+                                Array_Avg[minute][0] += 1
+                                first_time = False
+
+                            # add the fixation to current minute
+                            fixation_current = (minute + 1) * Time_Interval - int(fixation[1])
+                            Array_Avg[minute][1] += fixation_current
+
+                            # add portion of fixation to the next minute
+                            fixation_over = int(fixation[2]) - fixation_current
+                            new_key = minute + 1
+                            if new_key < Max_Time_Minutes:
+                                Array_Avg[new_key][1] += fixation_over
+    for x in range(Max_Time_Minutes):
+        if Array_Avg[x][0] > 0:
+            Array_Avg[x][1] = Array_Avg[x][1] / Array_Avg[x][0] / 1000
+
+    print(Array_Avg)
+    with open(target, 'w') as filehandle:
+        for listitem in Array_Avg:
+            filehandle.write('%s\n' % listitem)
+
 test_csv = 'Data\Processed\Graph_Expert\p1.graphFXD.csv'
 test_csv2 = 'Data\Processed\Graph_Expert\p3.graphFXD.csv'
 
@@ -11,91 +69,23 @@ Graph_General_Target = r"Data\Processed\Graph_General\\"
 Tree_Expert_Target = r"Data\Processed\Tree_Expert\\"
 Tree_General_Target = r"Data\Processed\Tree_General\\"
 
-Time_Interval = 60000 # one minute
-One_File_Averaged = True
-Start_Range = 0
-End_Range = Time_Interval
+Graph_E_target = r"data\Processed\Final\Graph_E_Final.txt"
+Graph_G_target = r"data\Processed\Final\Graph_G_Final.txt"
+Tree_E_target = r"data\Processed\Final\Tree_E_Final.txt"
+Tree_G_target = r"data\Processed\Final\Tree_G_Final.txt"
+
 # minute : [amount of people, average time for that minute]
-Value_Pair = [0,0]
-Participant_Counter = 0
-Max_Time = 0
-Min_Time = sys.maxsize
+Time_Interval = 60000  # one minute
 
-for filename in os.listdir(Graph_Expert_Target):
-    with open(Graph_Expert_Target + filename, newline='') as csvfile:
-        data = list(csv.reader(csvfile))
-    data = [x for x in data if x != []]
-    Max_Time = max(int(data[-1][1]), Max_Time)
-    Min_Time = min(int(data[-1][1]), Min_Time)
+Max_Time_Minutes = get_Max_Time(Graph_Expert_Target, Time_Interval)
+mk_Average_Fixation_File(Graph_Expert_Target, Graph_E_target, Max_Time_Minutes, Time_Interval)
 
-Max_Time_Minutes = math.ceil(Max_Time / Time_Interval)
-Min_Time_Minutes = math.ceil(Min_Time / Time_Interval)
+Max_Time_Minutes = get_Max_Time(Graph_General_Target, Time_Interval)
+mk_Average_Fixation_File(Graph_General_Target, Graph_G_target, Max_Time_Minutes, Time_Interval)
 
-# Average_Dictionary = dict.fromkeys(range(1, Max_Time_Minutes + 1))
-Average_Dictionary = {k: [0,0] for k in range(1, Max_Time_Minutes + 1)}
+Max_Time_Minutes = get_Max_Time(Tree_Expert_Target, Time_Interval)
+mk_Average_Fixation_File(Tree_Expert_Target, Tree_E_target, Max_Time_Minutes, Time_Interval)
 
-# Average_Dictionary[1] = [0,1]
-# print(Average_Dictionary)
-# print(Average_Dictionary[1])
-# print(Average_Dictionary[1][0])
-# print(Average_Dictionary[1][1])
+Max_Time_Minutes = get_Max_Time(Tree_General_Target, Time_Interval)
+mk_Average_Fixation_File(Tree_General_Target, Tree_G_target, Max_Time_Minutes, Time_Interval)
 
-for filename in os.listdir(Graph_Expert_Target):
-    with open(Graph_Expert_Target + filename, newline='') as csvfile:
-        data = list(csv.reader(csvfile))
-    data = [x for x in data if x != []]
-    # increment amount of people only once
-    for minute in range(1, Max_Time_Minutes + 1):
-        first_time = True
-        for fixation in data:
-            if int(fixation[1]) < (minute * Time_Interval):
-                if ((minute * Time_Interval) - Time_Interval) < int(fixation[1]) < (minute * Time_Interval):
-                    # print(str(minute) + ' ' + str(fixation) + ' ' + str(minute * Time_Interval - Time_Interval))
-                    # print(int(fixation[1]) + int(fixation[2]))
-                    # increment the amount of people for average
-                    if first_time:
-                        Average_Dictionary[minute][0] += 1
-                        first_time = False
-                    Average_Dictionary[minute][1] += int(fixation[2])
-                    print(Average_Dictionary[minute][1])
-                # else:
-                #     # increment the amount of people for average
-                #     if first_time:
-                #         Average_Dictionary[minute][0] += 1
-                #         first_time = False
-                #
-                #     # add the fixation to current minute
-                #     fixation_current = minute * Time_Interval - int(fixation[1])
-                #     Average_Dictionary[minute][1] += fixation_current
-                #
-                #     # add portion of fixation to the next minute
-                #     fixation_over = int(fixation[2]) - fixation_current
-                #     new_key = minute + 1
-                #     if new_key < Max_Time_Minutes:
-                #         Average_Dictionary[new_key][1] = fixation_over
-
-# with open(test_csv2, newline='') as csvfile:
-#     data = list(csv.reader(csvfile))
-# data = [x for x in data if x != []]
-# # increment amount of people only once
-# for minute in range(1, 3):
-#     first_time = True
-#     for fixation in data:
-#         if ((minute * Time_Interval) - Time_Interval) < int(fixation[1]) < (minute * Time_Interval):
-#             if (int(fixation[1]) + int(fixation[2])) < (minute * Time_Interval):
-#                 print(str(minute) + ' ' + str(fixation) + ' ' + str(minute * Time_Interval - Time_Interval))
-#                 print(int(fixation[1]) + int(fixation[2]))
-#                 # increment the amount of people for average
-#                 if first_time:
-#                     Average_Dictionary[minute][0] += 1
-#                     first_time = False
-#                 Average_Dictionary[minute][1] += int(fixation[2])
-#                 print(Average_Dictionary[minute][1])
-
-# take average for fixation duration (should always be less than Time_Interval)
-for x in range(1, Max_Time_Minutes + 1):
-    if Average_Dictionary[x][0] > 0:
-        Average_Dictionary[x][1] = Average_Dictionary[x][1] / Average_Dictionary[x][0] / 1000
-
-print(Average_Dictionary)
-print(Min_Time_Minutes)
