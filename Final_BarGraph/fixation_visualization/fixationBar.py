49d4ca91e4ff31ecff8784bf18cf8003a0ecfb02
Matthew Szeto <ms8288@gmail.com>
2019-11-13 21:32:12 -0800

    added final_bargraph folder

---
+++ Final_BarGraph/fixation_visualization/fixationBar.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+df = pd.read_csv("Graph_E_Finalc.csv")
+df1 = pd.read_csv("Tree_E_Finalc.csv")
+
+EGraph_minute = df['minute']
+EGraph_fixTime = df['average_fixation']
+EGraph_width = df['total_people']
+
+ETree_minute = df1['minute']
+ETree_fixTime = df1['average_fixation']
+ETree_width = df1['total_people']
+
+fig = go.Figure(
+    data=[go.Bar(x=EGraph_minute,y=EGraph_fixTime,width= EGraph_width/10),
+        go.Bar(x=ETree_minute, y=ETree_fixTime, width=ETree_width/10)],
+
+    layout=go.Layout(barmode='overlay')
+)
+
+fig.show()
+
