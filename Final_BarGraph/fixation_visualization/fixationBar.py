c22e89dd1930884b2364c86a3a659ec1044671c9
Matthew Szeto <ms8288@gmail.com>
2019-11-14 13:29:20 -0800

    updated hover mechanics

--- Final_BarGraph/fixation_visualization/fixationBar.py
+++ Final_BarGraph/fixation_visualization/fixationBar.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 
 
@@ -14,6 +13,8 @@ ETree_minute = df1['minute']
 ETree_fixTime = df1['average_fixation']
 ETree_width = df1['total_people']
 
+
+
 fig = go.Figure(
     data=[go.Bar(x=EGraph_minute,y=EGraph_fixTime,width= EGraph_width/10, showlegend=False),
         go.Bar(x=ETree_minute, y=ETree_fixTime, width=ETree_width/10, showlegend=False)],
@@ -22,18 +23,29 @@ fig = go.Figure(
 
 )
 
-fig.add_trace(go.Bar(x=EGraph_minute, y=EGraph_fixTime,width= EGraph_width/10,name="Expert Graph"))
+fig.add_trace(go.Bar(x=EGraph_minute, y=EGraph_fixTime,width= EGraph_width/10,
+                     name="Expert Graph",
+                     text=EGraph_width,
+                     hovertemplate="Minute: %{x} <br>Seconds: %{y}</br>Participants: %{text}"
+                     ))
 
-fig.add_trace(go.Bar(x=ETree_minute, y=ETree_fixTime, width=ETree_width/10,name="Expert Tree"))
+fig.add_trace(go.Bar(x=ETree_minute, y=ETree_fixTime, width=ETree_width/10,
+                     name="Expert Tree",
+                     text=ETree_width,
+                     hovertemplate="Minute: %{x} <br>Seconds: %{y}</br>Participants: %{text}"
+                     ))
 
 fig.update_layout(
     legend=go.layout.Legend(
         x=0,
-        y=1,
+        y=1
+    ),
+    title_text='Graph and Tree Fixation Times',
+    xaxis_title="Minutes",
+    yaxis_title="Fixation Time (seconds)"
 
-    )
 )
 
-
 fig.show()
+
 
